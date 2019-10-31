424613521470ce7af720b15b9ced79aee14f51bf
Thiha <waihynhtun1994@gmail.com>
2019-10-31 23:29:00 +0630

    update login design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import Signin from './components/signin/Signin';
 import Signup from './components/signup/Signup';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Home from './components/home/Home';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -28,29 +56,49 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-
-function App() {
-  const classes = useStyles();
+function HomeIcon(props) {
   return (
-    <div className="App">
-        <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                News
-              </Typography>
-              <Button color="inherit">Sign In</Button>
-              <Button color="inherit">Sign Up</Button>
-            </Toolbar>
-          </AppBar>
-          <Container fixed>
-              <Signin></Signin>
-              <Signup></Signup>
-          </Container>
-    </div>
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
   );
 }
 
-export default App;
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+       <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+             <HomeIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+             Hotel Admin
+          </Typography>
+          <Tabs value={value} onChange={handleChange} aria-label="Tabs with Navs">
+            <Tab label="Home" {...a11yProps(0)} />
+            <Tab label="Sign In" {...a11yProps(1)} />
+            <Tab label="Sign Up" {...a11yProps(2)} />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+           <Home/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+            <Signin/>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+            <Signup/>
+      </TabPanel>
+    </div>
+  );
+}
---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class extends Component{
+      render(){
+            return <div className='Home'>
+                 <h1>Welcome Home</h1>
+            </div>;
+      }
+}
